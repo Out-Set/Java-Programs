42236d3b9da86976e8bbb1ecc37832d2153af6bd
Out-Set <swk802@gmail.com>
2022-10-02 05:22:56 +0530

    on oct-02-2k22

--- 1-Java_Basic_Programs/src/Program17_FindSumOfFibonacciSeriesNumbersOfFirstNEvenIndexes.java
+++ 1-Java_Basic_Programs/src/Program17_FindSumOfFibonacciSeriesNumbersOfFirstNEvenIndexes.java
@@ -6,15 +6,10 @@ public class Program17_FindSumOfFibonacciSeriesNumbersOfFirstNEvenIndexes {
 }
 
 class SumOfFirstNEvenFibonacciNumber{
-    static void fib(int num){
-        int a = 0, b = 1, temp = 0;
-        System.out.println(a);
-        System.out.println(b);
-        for(int i=1; i<=num; i++){
-            System.out.println(b+a);
-            temp = a;
-            a = b;
-            b = temp;   
+    static int fib(int num){
+        if(num == 0){
+            return 0;
         }
+        return fib(num - 1)+fib(num - 2);
     }
 }
